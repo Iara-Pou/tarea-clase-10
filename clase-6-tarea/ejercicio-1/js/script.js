5ffa317f25b4ae9af3ed17e5726c5891b7c34c15
Iara Pou <iarapou21@hotmail.com>
2022-11-11 09:12:25 -0300

    Borra código cubierto por mostrar() y esconder()

--- clase-6-tarea/ejercicio-1/js/script.js
+++ clase-6-tarea/ejercicio-1/js/script.js
@@ -8,8 +8,7 @@ function guardarEdadesIntegrantes(numeros) {
 
 function manejarErroresCantidad(textoError, input) {
     vaciarErroresAnteriores();
-
-    $contenedorErrores.classList.remove("oculto");
+    mostrar($contenedorErrores)
 
     const contenedor = document.createElement("p");
     contenedor.textContent = textoError;
@@ -46,13 +45,14 @@ function manejarErroresEdades(erroresEdades) {
 
         if (error) {
             contadorErrores++;
-            $contenedorErrores.classList.remove("oculto");
+            mostrar($contenedorErrores)
             $contenedorErrores.textContent = "Algunos inputs tienen valores equivocados, por favor, revisalos."
-            
+
 
             conseguirInputPorValor(edad).forEach(input => {
                 input.classList.add("input-error");
 
+                //crea y suma texto debajo del input
                 let textoError = document.createElement("p");
                 textoError.textContent = error;
                 textoError.classList = "form-text text-danger"
@@ -80,10 +80,6 @@ function conseguirInputPorValor(valor) {
     return resultado;
 }
 
-function esconderErrores() {
-    $contenedorErrores.classList.add("oculto");
-}
-
 function vaciarErroresAnteriores() {
     $contenedorErrores.innerHTML = "";
     document.querySelectorAll(".form-text").forEach(texto => texto.remove())
@@ -93,18 +89,14 @@ function vaciarContenedorInputs() {
     $contenedorInputs.innerHTML = "";
 }
 
-function esconderMensajeFinal() {
-    document.querySelector("#mensaje").classList.add("oculto");
-}
-
 function esconderBotones() {
-    $botonCalcular.classList.add("oculto");
-    $botonReinicio.classList.add("oculto");
+    esconder($botonCalcular);
+    esconder($botonReinicio);
 }
 
 function mostrarBotones() {
-    $botonCalcular.classList.remove("oculto");
-    $botonReinicio.classList.remove("oculto");
+    mostrar($botonCalcular);
+    mostrar($botonReinicio);
 }
 
 const $botonCantidad = document.querySelector("#boton-cantidad");
@@ -112,8 +104,9 @@ const $botonCalcular = document.querySelector("#calculo-edades");
 const $botonReinicio = document.querySelector("#reinicio");
 const $botonReinicioGeneral = document.querySelector("#reinicio-general");
 
-let $contenedorInputs = document.querySelector("#contenedor-inputs-nuevos");
-let $contenedorErrores = document.querySelector("#errores");
+const $mensajeResultado = document.querySelector("#mensaje");
+const $contenedorInputs = document.querySelector("#contenedor-inputs-nuevos");
+const $contenedorErrores = document.querySelector("#errores");
 
 $botonCantidad.onclick = function () {
     vaciarContenedorInputs();
@@ -123,8 +116,8 @@ $botonCantidad.onclick = function () {
 
     if (esExito) {
         document.querySelector("#integrantes").classList.remove("input-error");
-        esconderErrores();
-        esconderMensajeFinal();
+        esconder($contenedorErrores);
+        esconder($mensajeResultado);
 
         crearInputLabels(cantidadIntegrantes);
         if ($botonCalcular.classList.contains("oculto")) {
@@ -141,9 +134,7 @@ $botonCantidad.onclick = function () {
 
 $botonCalcular.onclick = function () {
 
-    let $mensaje = document.querySelector("#mensaje");
     let edadesIntegrantes = guardarEdadesIntegrantes(document.querySelectorAll(".edad"));
-
     const erroresEdades = {};
 
     edadesIntegrantes.forEach(edad => {
@@ -153,12 +144,12 @@ $botonCalcular.onclick = function () {
     let esExito = manejarErroresEdades(erroresEdades) === 0;
 
     if (esExito) {
-        esconderErrores();
+        esconder($contenedorErrores);
         esconderBotones();
 
         document.querySelectorAll(".edad").forEach(elemento => elemento.classList.remove("input-error"));
 
-        $mensaje.classList.remove("oculto");
+        mostrar($mensajeResultado)
         document.querySelector("#edad-mayor").textContent = devolverNumeroMayor(edadesIntegrantes);
         document.querySelector("#edad-menor").textContent = devolverNumeroMenor(edadesIntegrantes);
         document.querySelector("#edad-promedio").textContent = devolverPromedio(edadesIntegrantes);
@@ -171,12 +162,12 @@ $botonReinicioGeneral.onclick = function () {
     document.querySelector("#integrantes").value = "";
     esconderBotones();
     vaciarContenedorInputs();
-    esconderMensajeFinal();
-    esconderErrores();
+    esconder($mensajeResultado);
+    esconder($contenedorErrores);
 
     return false;
 }
 
-$botonReinicio.onclick= function (){
-    document.querySelectorAll(".edad").forEach((inputEdad) => inputEdad.value="")
+$botonReinicio.onclick = function () {
+    document.querySelectorAll(".edad").forEach((inputEdad) => inputEdad.value = "")
 }
